81f0f6c1f7c804c792d09b820b4b0ca7854907e5
notquirrel <mudaf127@gmail.com>
2023-08-31 12:44:32 +0700

    finalization

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,56 +1,65 @@
-import CommunityCard from "@/components/cards/CommunityCard";
-import UserCard from "@/components/cards/UserCard";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import ThreadsTab from "@/components/shared/ThreadsTab";
-import { profileTabs } from "@/constants";
-import { fetchCommunities } from "@/lib/actions/community.actions";
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
-import Image from "next/image";
 import { redirect } from "next/navigation";
-import { use } from "react";
 
-async function Page() {
-    const user = await currentUser();
-    if(!user) return null;
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
+import CommunityCard from "@/components/cards/CommunityCard";
 
-    const userInfo = await fetchUser(user.id);
-    if(!userInfo?.onboarded) redirect('/onboarding');
+import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchCommunities } from "@/lib/actions/community.actions";
 
-    // Fetch communities
-    const result = await fetchCommunities({
-        searchString: '',
-        pageNumber: 1,
-        pageSize: 25,
-    })
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
 
-    return (
-        <section>
-            <h1 className="head-text mb-10">Search</h1>
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-            {/* Search bar */}
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
-            <div className="mt-14 flex flex-col gap-9">
-                {result.communities.length === 0 ? (
-                    <p className="no-result">No communities</p>
-                ) : (
-                    <>
-                        {result.communities.map((community) => (
-                            <CommunityCard
-                                key={community.id}
-                                id={community.id}
-                                name={community.name}
-                                username={community.username}
-                                imgUrl={community.image}
-                                bio={community.bio}
-                                members={community.members}
-                            />
-                        ))}
-                    </>
-                )}
-            </div>
-        </section>
-    )
+  return (
+    <>
+      <h1 className='head-text'>Communities</h1>
+
+      <div className='mt-5'>
+        <Searchbar routeType='communities' />
+      </div>
+
+      <section className='mt-9 flex flex-wrap gap-4'>
+        {result.communities.length === 0 ? (
+          <p className='no-result'>No Result</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      <Pagination
+        path='communities'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
+  );
 }
 
 export default Page;
--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,54 +1,62 @@
-import UserCard from "@/components/cards/UserCard";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import ThreadsTab from "@/components/shared/ThreadsTab";
-import { profileTabs } from "@/constants";
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
-import { currentUser } from "@clerk/nextjs";
-import Image from "next/image";
 import { redirect } from "next/navigation";
-import { use } from "react";
+import { currentUser } from "@clerk/nextjs";
 
-async function Page() {
-    const user = await currentUser();
-    if(!user) return null;
+import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
-    const userInfo = await fetchUser(user.id);
-    if(!userInfo?.onboarded) redirect('/onboarding');
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 
-    // Fetch users
-    const result = await fetchUsers({
-        userId: user.id,
-        searchString: '',
-        pageNumber: 1,
-        pageSize: 25,
-    })
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
 
-    return (
-        <section>
-            <h1 className="head-text mb-10">Search</h1>
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-            {/* Search bar */}
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
-            <div className="mt-14 flex flex-col gap-9">
-                {result.users.length === 0 ? (
-                    <p className="no-result">No users</p>
-                ) : (
-                    <>
-                        {result.users.map((person) => (
-                            <UserCard
-                                key={person.id}
-                                id={person.id}
-                                name={person.name}
-                                username={person.username}
-                                imgUrl={person.image}
-                                personType='User'
-                            />
-                        ))}
-                    </>
-                )}
-            </div>
-        </section>
-    )
+  return (
+    <section>
+      <h1 className='head-text mb-10'>Search</h1>
+
+      <Searchbar routeType='search' />
+
+      <div className='mt-14 flex flex-col gap-9'>
+        {result.users.length === 0 ? (
+          <p className='no-result'>No Result</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType='User'
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      <Pagination
+        path='search'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </section>
+  );
 }
 
 export default Page;
